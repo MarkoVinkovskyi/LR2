990818c27e1d7dda2fd9fc16d43bc36975b7be7b
MarkoVinkovskyi <marko.vinkovskyi@mipolytech.education>
2023-03-21 13:15:45 +0200

    Task_15_0.2

--- Task_15.py
+++ Task_15.py
@@ -1,4 +1,4 @@
-n = int(input())  # вводим число N
+n, *cards = map(int, input().split())
 sum_all = (n * (n + 1)) // 2  # вычисляем сумму всех номеров от 1 до N
-sum_left = sum(map(int, input().split()))  # считываем и вычисляем сумму номеров оставшихся карточек
+sum_left = sum(cards)  # вычисляем сумму номеров оставшихся карточек
 print(sum_all - sum_left)  # выводим номер потерянной карточки
