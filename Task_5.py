fe2b662a3eaac60cacb6b3a08dd6aaf6a2aefd41
MarkoVinkovskyi <marko.vinkovskyi@mipolytech.education>
2023-03-21 10:19:16 +0200

    Task_5_0.1

--- Task_5.py
+++ Task_5.py
@@ -1,8 +1,11 @@
 def f(num):
     m=0
+    a=""
+    if num<0 : a="-"
+    num=abs(num)
     while num>0:
         m = m*10+num%10
         num = num//10
-    return m
+    return a+str(m)
 number = int(input())
 print(f(number))
