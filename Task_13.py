e5d56a0b6af2db29e48544b55857ae83b1a616a0
Marko <marko.vinkovskyi@mipolitech.education>
2023-03-23 20:56:57 +0200

    Task_13_0.6

--- Task_13.py
+++ Task_13.py
@@ -1,7 +1,13 @@
-c=-1002
+
+
+maxOne=-1001
+maxTwo=-1002
 number = int(input())
 while number != 0:
-    if c<number:
-       c=number
+    if maxOne<number:
+       maxTwo=maxOne
+       maxOne=number
+    elif maxTwo<number:
+       maxTwo=number
     number = int(input())
-print(c)
+print(maxTwo)
