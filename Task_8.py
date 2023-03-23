6ec006b1523103ab25365a08784c42002be4058c
Marko <marko.vinkovskyi@mipolitech.education>
2023-03-23 22:01:48 +0200

    Task_8_0.4

--- Task_8.py
+++ Task_8.py
@@ -1,12 +1,16 @@
-X = float(input())
-M=0
-N=0
-K=0
-while K < X:
-   N = N + 1
-   K=K+(1/N)
-
-if K > X:
-   M=N+1
-else: M=N+2
-print((N-1),(M-1))
+x = float(input())
+eps = 1e-6
+n = 1
+sum_n = 1.0
+# ищем максимальное n, при котором сумма меньше x
+while sum_n - x < eps:
+    n += 1
+    sum_n += 1.0 / n
+m = 1
+sum_m = 1.0
+# ищем минимальное m, при котором сумма больше x
+while x - sum_m > eps:
+    m += 1
+    sum_m += 1.0 / m
+# выводим результаты
+print(n - 1, m)
