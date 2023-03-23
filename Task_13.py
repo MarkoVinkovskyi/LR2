138677985474c35ed21dc47b29d71234d8abd0c9
Marko <marko.vinkovskyi@mipolitech.education>
2023-03-23 20:20:27 +0200

    Task_13_0.2

--- Task_13.py
+++ Task_13.py
@@ -1,13 +1,7 @@
 c=0
-f=0
-k=0
-while 1:
-   n =int(input())
-   if n != 0:
-      if abs(n)>abs(k):
-         f = c
-         c = n
-      k = n
-   elif n == 0:
-      break
-print(f)
+number = int(input())
+while number != 0:
+    if c<abs(number):
+       c=number
+    number = int(input())
+print(c)
