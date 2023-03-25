56cd21fb9ecbf8da328c493d5ad8f4e7cf9649de
Marko <marko.vinkovskyi@mipolytech.education>
2023-03-25 17:01:16 +0200

    Task_4 1.1

--- Task_4.py
+++ Task_4.py
@@ -1,6 +1,6 @@
-K=int(input())
-max_num = (9999 // K) * K
-min_num = K - (1000 % K) + 1000
-n = (max_num - min_num) // K + 1
-sum = (min_num + max_num) * n // 2
+K = int(input())
+sum = 0
+for num in range(K, 10000, K):
+    if num>999:
+        sum += num
 print(sum)
