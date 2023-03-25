3843ed46fb0aaa43c61570af0027a60c9f13b934
Marko <marko.vinkovskyi@mipolytech.education>
2023-03-25 17:38:48 +0200

    Task_8 1.2

--- Task_8.py
+++ Task_8.py
@@ -2,12 +2,13 @@ X = float(input())
 M=0
 N=0
 K=0
-while K < X:
+eps=1e-6
+while K < X-eps:
    N = N + 1
    K=K+(1/N)
 M=N
 while 1:
-   if K<=X:
+   if K<=X+eps:
       M = M + 1
       K = K + (1 / M)
    else:
