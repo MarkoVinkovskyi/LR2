bfe4c8150e52154a3b2d0ee6b277e01bb616d63f
Marko <marko.vinkovskyi@mipolytech.education>
2023-03-25 17:01:29 +0200

    Task_7 1.1

--- Task_7.py
+++ Task_7.py
@@ -3,6 +3,6 @@ N,P,M = map(int,input().split())
 c=0
 while N<M:
    N=N+N*(P/100)
-   N = math.floor(N * 100) / 100  # округляем до 2 знаков после запятой
+   N = math.floor(N)   # округляем до 2 знаков после запятой
    c +=1
 print(c)
