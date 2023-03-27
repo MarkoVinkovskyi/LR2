45d4ebe861038e87d6f3f979ed875d19f8772657
Marko <marko.vinkovskyi@mipolytech.education>
2023-03-27 14:26:28 +0300

    Task_15 2.0

--- Task_15.py
+++ Task_15.py
@@ -1,4 +1,8 @@
+
+def find_missing_card(N, Cards):
+    expected_sum = (N * (N + 1)) // 2
+    actual_sum = sum(Cards)
+    return expected_sum - actual_sum
+
 n, *cards = map(int, input().split())
-sum_all = (n * (n + 1)) // 2  # вычисляем сумму всех номеров от 1 до N
-sum_left = sum(cards)  # вычисляем сумму номеров оставшихся карточек
-print(sum_all - sum_left)  # выводим номер потерянной карточки
+print(find_missing_card(n, cards))
